382bb20e68583a73c31c5a538cc87709a156adc6
tmgsilva3 <124098319+tmgsilva3@users.noreply.github.com>
2024-01-31 16:22:00 +0000

    Update Calculator.py
    Added to code import math module, raw input for choice and if, elif and else

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,17 @@
+from math import *
+
 print("""Hello! Welcome to the Calculator Game
 Here you can test your Math skills and practice all sorts of arithmetic
 You can choose many levels and the numbers are generated randomly (available soon!)
 Enjoy!""")
+
+c = raw_input("Would you like random numbers or fixed numbers? Choose 'random' or 'fixed'")
+
+if(c.lower() == 'random'):
+  
+
+elif(c.lower() == 'fixed'):
+
+
+else:
+  print("Choose a valid choice.")
