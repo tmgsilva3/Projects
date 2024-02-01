5b9f8091d181b6838acb900f1ed2723cafe3b19e
tmgsilva3 <124098319+tmgsilva3@users.noreply.github.com>
2024-02-01 09:41:39 +0000

    Update Calculator.py
    Changed functions to top of the file; added print function to print random generated numbers

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,20 @@
 import random
 
+def randarith():
+  a = random.randint(1,10)
+  b = random.randint(1,10)
+  print("The numbers are " + str(a) + " and " + str(b))
+
+def numarith():
+  passs
+  
+
 print("""Hello! Welcome to the Calculator Game
 Here you can test your Math skills and practice all sorts of arithmetic
 You can choose many levels and the numbers are generated randomly (available soon!)
 Enjoy!""")
 
-c = raw_input("Would you like random numbers or fixed numbers? Choose 'random' or 'fixed'")
+c = input("Would you like random numbers or fixed numbers? Choose 'random' or 'fixed'")
 
 if(c.lower() == 'random'):
   randarith()
@@ -15,11 +24,3 @@ elif(c.lower() == 'fixed'):
 
 else:
   print("Choose a valid choice.")
-
-
-def randarith():
-  a = random.randit(1,10)
-  b = random.randit(1,10)
-
-def numarith():
-  passs
