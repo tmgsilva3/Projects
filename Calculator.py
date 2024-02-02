a0b68d563ee81a5c43fe357ab2357071fa6f999a
tmgsilva3 <124098319+tmgsilva3@users.noreply.github.com>
2024-02-02 15:18:39 +0000

    Update Calculator.py
    Added a repeat function to calculator

--- Calculator.py
+++ Calculator.py
@@ -17,9 +17,25 @@ def numarith():
   c = a + b
   answer = int(input("{} + {} = ".format(a, b)))
   if (c == answer):
-      print("You got it!")  
+      print("You got it!")
   else:
       print("Incorrect!")
+      
+      
+def repeat():
+    answer = input("Do you want to go again? Yes or No?\n")
+    if(answer.lower() == 'yes'):
+        answer2 = input("Which mode 'Random' or 'Fixed' mode?\n")
+        if(answer2.lower() == 'random'):
+            randarith()
+            repeat()
+        elif(answer2.lower() == 'fixed'):
+            numarith()
+            repeat()
+        else:
+            print("Choose a valid choice.")
+    elif(answer.lower() == 'no'):
+        pass
   
 
 print("""Hello! Welcome to the Calculator Game
@@ -27,7 +43,7 @@ Here you can test your Math skills and practice all sorts of arithmetic
 You can choose many levels and the numbers are generated randomly (available soon!)
 Enjoy!""")
 
-c = input("Would you like random numbers or fixed numbers? Choose 'random' or 'fixed'")
+c = input("Would you like random numbers or fixed numbers? Choose 'random' or 'fixed'\n")
 
 if(c.lower() == 'random'):
   randarith()
@@ -37,3 +53,5 @@ elif(c.lower() == 'fixed'):
 
 else:
   print("Choose a valid choice.")
+  
+repeat()
