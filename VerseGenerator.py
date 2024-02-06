e1891966f6bddc5db909323c579fe911e6153977
tmgsilva3 <tmgsilva3@gmail.com>
2024-02-06 00:19:43 +0000

    Added names of books of the New Testament

--- VerseGenerator.py
+++ VerseGenerator.py
@@ -1,1 +1,70 @@
-genesis = {}
+def old():
+	genesis = {:}
+	exodus = {:}
+	leviticus = {:}
+	numbers = {:}
+	deuteronomy = {:}
+	joshua = {:}
+	judges = {:}
+	ruth = {:}
+	samuel1 = {:}
+	samuel2 = {:}
+	kings1 = {:}
+	kings2 = {:}
+	chronicles1 = {:}
+	chronicles2 = {:}
+	ezra = {:}
+	nehemiah = {:}
+	esther = {:}
+	job = {:}
+	psalms = {:}
+	proverbs = {:}
+	ecclesiastes = {:}
+	songofsalomon = {:}
+	isaiah = {:}
+	jeremiah = {:}
+	lamentations = {:}
+	ezekiel = {:}
+	daniel = {:}
+	hosea = {:}
+	joel = {:}
+	amos = {:}
+	obadiah = {:}
+	jonah = {:}
+	micah = {:}
+	nahum = {:}
+	habakkuk = {:}
+	zephaniah = {:}
+	haggai = {:}
+	zechariah = {:}
+	malachi = {:}
+
+
+def new():
+	matthew
+	mark
+	luke
+	john
+	acts
+	romans
+	corinthians1
+	corinthians2
+	galatians
+	ephesians
+	philippians
+	colossians
+	thessalonians1
+	thessalonians2
+	timothy1
+	timothy2
+	titus
+	philemon
+	hebrews
+	james
+	peter
+	peter2
+	john1
+	john2
+	john3
+	jude
+	revelation
