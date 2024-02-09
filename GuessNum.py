471ea4483b01f5e1c45bb06120029a9361a6fbf2
tmgsilva3 <tmgsilva3@gmail.com>
2024-02-09 00:07:14 +0000

    Added isdigit function and if else to verify if answer equals guess

--- GuessNum.py
+++ GuessNum.py
@@ -2,5 +2,17 @@ import random
 
 print("Welcome to 'Guess The Number'! Let's play!")
 
-answer 
-guess = int(input("Enter your guess between 1 and 10: "))
+answer = random.randint(1, 10)
+guess = input("Enter your guess between 1 and 10: ")
+
+print("The random number is: " + str(answer))
+
+if (guess.isdigit()):
+	if (guess == answer):
+		print("You got it right!")
+		quit()
+	else:
+		print("Try again next time!")
+		quit()
+else:
+	print("Insert a number next time!")
