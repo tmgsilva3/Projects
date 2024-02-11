e9fa1e28ce3e13c664436c43ab148361a6dacfe1
tmgsilva3 <124098319+tmgsilva3@users.noreply.github.com>
2024-02-11 14:58:12 +0000

    Update VerseGenerator.py
    Added Genesis' First Chapter's 4 verses inside a nested dictionary

--- VerseGenerator.py
+++ VerseGenerator.py
@@ -1,5 +1,12 @@
 def old():
-	genesis = {:}
+	genesis = {
+		1: {
+			1: "In the beginning God created the heaven and the earth.",
+			2: "And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters",
+			3: "And God said, Let there be light: and there was light.",
+			4: "And God saw the light, that it was good: and God divided the light from the darkness."
+		}
+	}
 	exodus = {:}
 	leviticus = {:}
 	numbers = {:}
